d1beadf54646abcf8a98fe2d40ae194b5bda20de
ahsan <ahsanrabby958@gmail.com>
2024-05-30 17:57:38 +0700

    Push code to remote repository

---
+++ src/server/routes.js
@@ -0,0 +1,26 @@
+const {
+    postPredictHandler,
+    getPredictHandler,
+  } = require("../server/handler");
+  
+  const routes = [
+    {
+      path: "/predict",
+      method: "POST",
+      handler: postPredictHandler,
+      options: {
+        payload: {
+          allow: "multipart/form-data",
+          maxBytes: 1000000,
+          multipart: true,
+        },
+      },
+    },
+    {
+      path: "/predict/histories",
+      method: "GET",
+      handler: getPredictHandler,
+    },
+  ];
+  
+  module.exports = routes;
